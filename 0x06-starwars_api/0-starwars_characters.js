c2aec486c6c28bde0533b18fea8eb8a4265a5ef5
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-08-09 03:48:15 -0700

    Task 0. Write a script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const Url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(url, async (err, res, body) => {
   err && console.log(err);

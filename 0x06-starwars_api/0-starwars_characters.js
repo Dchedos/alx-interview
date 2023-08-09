de95146b63cf8f9c673f9ac932cd02887b5af7f2
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-08-09 02:28:52 -0700

    Task 0. Write a script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,37 +4,20 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(Url, async (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+request(url, async (err, res, body) => {
+  err && console.log(err);
 
-  if (response.statusCode !== 200) {
-    console.error(`Request failed with status code ${response.statusCode}`);
-    return;
-  }
+  const charactersArray = (JSON.parse(res.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        err && console.log(err);
 
-  const filmData = JSON.parse(body);
-  const charactersUrls = filmData.characters;
-
-  charactersUrls.forEach(characterUrl => {
-    request(characterUrl, (characterError, characterResponse, characterBody) => {
-      if (characterError) {
-        console.error(characterError);
-        return;
-      }
-
-      if (characterResponse.statusCode !== 200) {
-        console.error(`Request failed with status code ${characterResponse.statusCode}`);
-        return;
-      }
-
-      const characterData = JSON.parse(characterBody);
-      console.log(characterData.name);
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  });
+  }
 });
-

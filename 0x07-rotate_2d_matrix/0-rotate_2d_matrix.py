e0f02e1bc873e613c57ffe2246579b3a66ded9e6
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-08-15 00:04:33 -0700

    Task 0. Given an n x n 2D matrix, rotate it 90 degrees clockwise

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -18,3 +18,17 @@ def rotate_2d_matrix(matrix):
             matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
             matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
             matrix[j][n - i - 1] = temp
+
+
+"""
+# Example usage
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    rotate_2d_matrix(matrix)
+    for row in matrix:
+        print(row)
+"""
